71bad274d8731e76af4dbd2f452b88dd3ffebe56
Qais Ziaey <qziaey@gmail.com>
2024-02-23 15:08:31 +0100

    add css style to the todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,59 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    async function getTodos() {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "https://todo-express-js-henna.vercel.app"
+        );
+        setTodos(response.data);
+        console.log(response.data);
+      } catch (error) {
+        setErrorMessage("Error retrieving todos. Please try again later.");
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getTodos();
+  }, []);
+
   return (
     <div>
-      <h1>Todo App</h1>
+      <h1 className="app-title">Todo App</h1>
       <p>Manage your tasks with ease</p>
       <div className="todos-main-container">
         <div className="users-buttons-container">
-          <button>Tom</button>
-          <button>Sam</button>
-          <button>Bob</button>
-          <button>Tim</button>
-          <button>John</button>
+          <a href="#">Sarah</a>
+          <a href="#">Matt</a>
+          <a href="#">Lisa</a>
+          <a href="#">Joe</a>
+          <a href="#">Alex</a>
         </div>
-        <div className="list-container">list of users</div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : todos ? (
+          <>
+            <div className="list-container">
+              <div className="todo-item">
+                <ul>
+                  <li>some to do</li>
+                  {todos.map((todo) => (
+                    <li key={todo.id}>{todo.content}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </>
+        ) : null}
       </div>
     </div>
   );
