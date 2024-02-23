83f193009fa69770c4f3328e63154a5a92f7fbd2
Qais Ziaey <qziaey@gmail.com>
2024-02-23 12:31:09 +0100

    add content on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import axios from "axios";
 
 function App() {
   return (
-    <>
+    <div>
       <h1>Todo App</h1>
-    </>
+      <p>Manage your tasks with ease</p>
+      <div className="todos-main-container">
+        <div className="users-buttons-container">
+          <button>Tom</button>
+          <button>Sam</button>
+          <button>Bob</button>
+          <button>Tim</button>
+          <button>John</button>
+        </div>
+        <div className="list-container">list of users</div>
+      </div>
+    </div>
   );
 }
 
